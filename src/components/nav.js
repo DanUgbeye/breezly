a3bdbb167d9b3b36a11b11268d9b46fddb4c4b17
DanUgbeye <82518175+DanUgbeye@users.noreply.github.com>
2022-03-21 15:38:41 +0100

    Initial Commit

---
+++ src/components/nav.js
@@ -0,0 +1,16 @@
+
+const Nav = ({name}) => {
+
+  return (
+    <nav>
+      <h2>Breezly</h2>
+      <ul>
+        <li>Home</li>
+        <li>News</li>
+        <li>Motivation</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav;
